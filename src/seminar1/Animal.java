48da290d9ebc3e122c9e2e0c7b6a53b5ec81dce7
ilinq01 <89912932+ilinq01@users.noreply.github.com>
2022-02-23 18:56:22 +0200

    semianr 1

---
+++ src/seminar1/Animal.java
@@ -0,0 +1,19 @@
+package seminar1;
+
+public abstract class Animal {
+    private String nume;
+
+    public abstract void mananca(String tipHrana);
+
+    public String getNume() {
+        return nume;
+    }
+
+    public void setNume(String nume) {
+        this.nume = nume;
+    }
+
+    public Animal(String nume) {
+        this.nume = nume;
+    }
+}
---
+++ src/seminar1/IngrijitorZoo.java
@@ -0,0 +1,9 @@
+package seminar1;
+
+public class IngrijitorZoo {
+    private String nume;
+
+    public void hranesteAnimal(Animal animal, String mancare) {
+        animal.mananca(mancare);
+    }
+}

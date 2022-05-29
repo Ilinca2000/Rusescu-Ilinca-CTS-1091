5fc06dd82f2081e16c872d1ba18b9b26b67f4ec6
ilinq01 <89912932+ilinq01@users.noreply.github.com>
2022-05-29 17:36:13 +0300

    clase memento seminar 10

---
+++ seminar10/src/memento/clase/ManagerAutobuz.java
@@ -0,0 +1,18 @@
+package memento.clase;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ManagerAutobuz {
+    List<AutobuzMemento> autobuze;
+
+    public ManagerAutobuz(){
+        autobuze= new ArrayList<>();
+    }
+    public void adaugaMemento(AutobuzMemento memento){
+        autobuze.add(memento);
+    }
+    public AutobuzMemento getMemento(Integer index){
+        return autobuze.get(index);
+    }
+}

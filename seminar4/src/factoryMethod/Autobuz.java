4fe5bea7aee9dc9ee9b8f238b5e11944cb005666
ilinq01 <89912932+ilinq01@users.noreply.github.com>
2022-05-29 16:23:06 +0300

    clase adaugate seminar 4

--- seminar4/src/factoryMethod/Autobuz.java
+++ seminar4/src/factoryMethod/Autobuz.java
@@ -1,12 +1,16 @@
 package factoryMethod;
 
-public class Autobuz extends MijlocTransportComun {
+public class Autobuz extends MijlocTransportComun{
+
     public Autobuz(String nrInmatriculare) {
         super(nrInmatriculare);
     }
 
     @Override
     public String toString() {
-        return "Autobuz{}";
+        final StringBuilder sb = new StringBuilder("Autobuz{");
+        sb.append("nrInmatriculare='").append(nrInmatriculare).append('\'');
+        sb.append('}');
+        return sb.toString();
     }
 }
